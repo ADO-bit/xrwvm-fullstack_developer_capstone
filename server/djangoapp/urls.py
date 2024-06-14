95c5af265fe28bb6efb661c4a749a9a3c3b62053
ADO-bit <138983231+ADO-bit@users.noreply.github.com>
2024-06-14 23:52:51 +0200

    Update urls.py

--- server/djangoapp/urls.py
+++ server/djangoapp/urls.py
@@ -1,10 +1,10 @@
-# Uncomment the imports before you add the code
 from django.urls import path
 from django.conf.urls.static import static
 from django.conf import settings
 from . import views
 
 app_name = 'djangoapp'
+
 urlpatterns = [
     # path for registration
     path(route='register', view=views.registration, name='register'),
