6d394050d20786e2590d415979fe2e596070d0fb
ADO-bit <138983231+ADO-bit@users.noreply.github.com>
2024-06-14 23:53:48 +0200

    Update restapis.py

--- server/djangoapp/restapis.py
+++ server/djangoapp/restapis.py
@@ -4,21 +4,18 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-backend_url = os.getenv(
-    'backend_url', default="http://localhost:3030")
-sentiment_analyzer_url = os.getenv(
-    'sentiment_analyzer_url',
-    default="http://localhost:5050/")
+backend_url = os.getenv('backend_url', default="http://localhost:3030")
+sentiment_analyzer_url = os.getenv('sentiment_analyzer_url', default="http://localhost:5050/")
 
 def get_request(endpoint, **kwargs):
     params = ""
     if kwargs:
         for key, value in kwargs.items():
-            params = params + key + "=" + value + "&"
+            params += f"{key}={value}&"
 
-    request_url = backend_url + endpoint + "?" + params
-
-    print("GET from {} ".format(request_url))
+    request_url = f"{backend_url}{endpoint}?{params}"
+    print(f"GET from {request_url}")
+    
     try:
         response = requests.get(request_url)
         return response.json()
@@ -27,7 +24,8 @@ def get_request(endpoint, **kwargs):
         print("Network exception occurred")
 
 def analyze_review_sentiments(text):
-    request_url = sentiment_analyzer_url + "analyze/" + text
+    request_url = f"{sentiment_analyzer_url}analyze/{text}"
+    
     try:
         response = requests.get(request_url)
         return response.json()
@@ -36,7 +34,8 @@ def analyze_review_sentiments(text):
         print("Network exception occurred")
 
 def post_review(data_dict):
-    request_url = backend_url + "/insert_review"
+    request_url = f"{backend_url}/insert_review"
+    
     try:
         response = requests.post(request_url, json=data_dict)
         print(response.json())
