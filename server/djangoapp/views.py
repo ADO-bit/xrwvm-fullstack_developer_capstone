ee9ebad2c2dbeed26fa0242e0c991e4418d96f25
ADO-bit <138983231+ADO-bit@users.noreply.github.com>
2024-06-15 00:21:26 +0200

    Update views.py

--- server/djangoapp/views.py
+++ server/djangoapp/views.py
@@ -56,7 +56,7 @@ def registration(request):
 
     if not username_exist:
         user = User.objects.create_user(
-            username=username, first_name=first_name, last_name=last_name, 
+            username=username, first_name=first_name, last_name=last_name,
             password=password, email=email)
         login(request, user)
         data = {"userName": username, "status": "Authenticated"}
